eefcad492c7aaf0208685ee93e838c4722025d4c
MahdiT54 <mahditanzim2@gmail.com>
2023-11-25 23:16:58 -0500

    linked movieinfo

--- src/pages/MovieInfo.jsx
+++ src/pages/MovieInfo.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const MovieInfo = () => {
-    return (
+  const { id } = useParams();
+  const [movieInfo, setMovieInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchMovieInfo = async () => {
+      const response = await axios.get(
+        `https://www.omdbapi.com/?i=${id}&apikey=1c44ead`
+      );
+      setMovieInfo(response.data);
+    };
+    fetchMovieInfo();
+  }, [id]);
+
+  return (
+    <div>
+      {movieInfo ? (
         <div>
-            
+          <h1>{movieInfo.Title}</h1>
+          {/* Display other movie information as needed */}
         </div>
-    );
-}
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
 
 export default MovieInfo;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Notfound from "../assets/image_not_found.png";
 import Searchbox from "../components/Searchbox";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Movies = () => {
   const location = useLocation();
@@ -24,21 +24,27 @@ const Movies = () => {
         <div className="movies__box">
           {movies.length > 0 ? (
             movies.map((movie) => (
-              <div className="movie-card" key={movie.imdbID} onClick={() => console.log('ey o')}>
-                <div className="movie-card__container">
-                  <div className="search-item-thumbnail">
-                    <im
-                      src={movie.Poster !== "N/A" ? movie.Poster : Notfound}
-                      className="search-item-img"
-                      alt="movie poster"
-                    />
-                  </div>
-                  <div className="search-item-info">
-                    <h3 className="search-item-h3">{movie.Title}</h3>
-                    <p className="search-item-p">{movie.Year}</p>
+              <Link to={`/movies/${movie.imdbID}`}>
+                <div
+                  className="movie-card"
+                  key={movie.imdbID}
+                  onClick={() => console.log("ey o")}
+                >
+                  <div className="movie-card__container">
+                    <div className="search-item-thumbnail">
+                      <img
+                        src={movie.Poster !== "N/A" ? movie.Poster : Notfound}
+                        className="search-item-img"
+                        alt="movie poster"
+                      />
+                    </div>
+                    <div className="search-item-info">
+                      <h3 className="search-item-h3">{movie.Title}</h3>
+                      <p className="search-item-p">{movie.Year}</p>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Link>
             ))
           ) : (
             <h2>{`You searched for "${searchTerm}" - No movies found.`}</h2>

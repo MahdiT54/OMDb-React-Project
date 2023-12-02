26f27185144a85ec6215ea804debd4dbdd45a0c8
MahdiT54 <mahditanzim2@gmail.com>
2023-12-02 10:42:59 -0500

    about section updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Undrawabout from "../assets/undraw_online_video_re_fou2.svg";
-import Undrawinterface from "../assets/undraw_usability_testing_re_uu1g.svg"
+import Undrawinterface from "../assets/undraw_usability_testing_re_uu1g.svg";
+import Undrawwebdevices from "../assets/undraw_web_devices_re_m8sc (1).svg"
 
 
 const About = () => {
   return (
     <section id="about__page" className="up__padding">
-      <div className="row about-row">
+      <div className="row about__row">
         <div className="about__header--left">
-          <h1 className="about__header--left-h1">What is OMDbGo?</h1>
+          <h1 className="about__header--left-h1">
+            What is OMDb<span className="teal-gradient">Go</span>?
+          </h1>
           <p className="about__header--left-p">
             We harness the power of the OMDb API to provide you with up-to-date
             and accurate information about a vast array of movies and shows.
@@ -22,10 +25,12 @@ const About = () => {
           <img src={Undrawabout} className="about__header--right-img" alt="" />
         </figure>
       </div>
-      <div className="row interface-row">
+      <div className="row interface__row">
         <div className="interface__left">
-          <h1 className="about__header--left-h1">User-Friendly Interface</h1>
-          <p className="about__header--left-p">
+          <h1 className="interface__header--left-h1">
+            User-Friendly <span className="teal-gradient">Interface</span>
+          </h1>
+          <p className="interface__header--left-p">
             Our clean and user-friendly interface ensures a smooth and enjoyable
             browsing experience. Navigate through the world of entertainment
             effortlessly, with a focus on simplicity and functionality.
@@ -35,17 +40,21 @@ const About = () => {
           <img src={Undrawinterface} alt="" />
         </figure>
       </div>
+      <div className="row responsive__row">
+        <div className="responsive__left">
+          <h1 className="responsive__header--left-h1"><span className="teal-gradient">Responsive</span> Design</h1>
+          <p className="responsive__header--left-p">
+            Enjoy OMDbGo on any device – desktop, tablet, or mobile. Our website
+            is optimized for responsiveness, providing you with a seamless
+            experience regardless of your preferred device.
+          </p>
+        </div>
+        <figure className="responsive__right">
+          <img src={Undrawwebdevices} alt="" />
+        </figure>
+      </div>
     </section>
   );
 };
 
 export default About;
-
-{
-  /* <h1>Responsive Design</h1>
-<p>
-  Enjoy OMDbGo on any device – desktop, tablet, or mobile. Our website
-  is optimized for responsiveness, providing you with a seamless
-  experience regardless of your preferred device.
-</p> */
-}

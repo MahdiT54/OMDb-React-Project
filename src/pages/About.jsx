1122df639ac6f2218c61d01a9f52563143731d6f
MahdiT54 <mahditanzim2@gmail.com>
2023-12-01 21:38:23 -0500

    user-interface section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Undrawabout from "../assets/undraw_online_video_re_fou2.svg";
+import Undrawinterface from "../assets/undraw_usability_testing_re_uu1g.svg"
+
 
 const About = () => {
   return (
@@ -20,22 +22,30 @@ const About = () => {
           <img src={Undrawabout} className="about__header--right-img" alt="" />
         </figure>
       </div>
+      <div className="row interface-row">
+        <div className="interface__left">
+          <h1 className="about__header--left-h1">User-Friendly Interface</h1>
+          <p className="about__header--left-p">
+            Our clean and user-friendly interface ensures a smooth and enjoyable
+            browsing experience. Navigate through the world of entertainment
+            effortlessly, with a focus on simplicity and functionality.
+          </p>
+        </div>
+        <figure className="interface__right">
+          <img src={Undrawinterface} alt="" />
+        </figure>
+      </div>
     </section>
   );
 };
 
 export default About;
 
-{/* <h1>User-Friendly Interface</h1>
-<p>
-  Our clean and user-friendly interface ensures a smooth and enjoyable
-  browsing experience. Navigate through the world of entertainment
-  effortlessly, with a focus on simplicity and functionality.
-</p> */}
-
-{/* <h1>Responsive Design</h1>
+{
+  /* <h1>Responsive Design</h1>
 <p>
   Enjoy OMDbGo on any device – desktop, tablet, or mobile. Our website
   is optimized for responsiveness, providing you with a seamless
   experience regardless of your preferred device.
-</p> */}
+</p> */
+}

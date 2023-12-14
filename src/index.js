d60091a76dea82c682dc03b53298dc00c4bfe404
MahdiT54 <mahditanzim2@gmail.com>
2023-12-14 01:03:53 -0500

    undo strictmode

--- src/index.js
+++ src/index.js
@@ -31,7 +31,11 @@ library.add(
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
